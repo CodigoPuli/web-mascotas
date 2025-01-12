f2b184620876f41de73bd6662b9adf1378674ccd
Javier Pulido <jpdgranada@gmail.com>
2025-01-12 22:16:31 +0100

    Pet web project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Función para abrir la imagen en tamaño completo
+function openImage(img) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-img");
+  modal.style.display = "flex"; // Muestra el modal
+  modalImg.src = img.src; // Establece la fuente de la imagen en el modal
+}
+
+// Función para cerrar el modal
+function closeImage() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none"; // Oculta el modal
+}
